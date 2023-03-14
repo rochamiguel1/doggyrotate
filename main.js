6b959af1768733932c5158f75014942ae0d368fe
rochamiguel1 <miroga2005@gmail.com>
2023-03-14 16:17:46 +0100

    martes

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
-const btnRotate = document.querySelector (".btnRotate")
-const btnBack = document.querySelector(".btnBack")
-const contenedor2 = document.querySelector(".contenedor2")
-const buttons = document.querySelector(".buttons")
-const nav = document.querySelector("nav")
-btnRotate.addEventListener ("click" ,()=> {
-contenedor2.style.transform= `rotate(-20deg)`
-contenedor2.style.transition = `1s`
-buttons.style.transform= `rotate(-70deg)`
-nav.style.display = `block`
 
-})
-btnBack.addEventListener("click", ()=> {
-contenedor2.style.transform = `rotate(0)`
-buttons.style.transform= `rotate(0)`
-nav.style.display = `none`
+const circulo = document.querySelector(".circulo")
+const contenedor = document.querySelector(".contenedor")
 
-})
+
+function doggyrotate() {
+    circulo.classList.toggle("rotate")
+    contenedor.classList.toggle("turn")
+
+}
